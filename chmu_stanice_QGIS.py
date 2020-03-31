77879494f926e57926f66040f1bd22b15d770f2b
Oto Kaláb <kalab.oto@gmail.com>
2020-03-31 14:37:12 +0200

    fix:removed remains

--- chmu_stanice_QGIS.py
+++ chmu_stanice_QGIS.py
@@ -8,7 +8,6 @@ for file_nam in js_names:
     for l in range(1,len(str_data)-1):
         lay_nam = str(str_data[l].split(b"=")[0][4:-1], 'utf-8')
         geojs = str(str_data[l].split(b"=")[1], 'utf-8')
-        QgsVectorLayer
         iface.addVectorLayer(geojs, lay_nam, "ogr")
 
 # meta = urlopen("http://portal.chmi.cz/files/portal/docs/poboc/OS/stanice/js/messages.js").readlines()
