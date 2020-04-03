376fae95c6d9b3cd5bf64a4409a27a968a855fd4
datastory.cz <honza@datastory.cz>
2020-04-03 12:07:34 +0200

    portace pod Win, data v csv, kopie dat v repu

--- chmu_stanice.py
+++ chmu_stanice.py
@@ -1,27 +1,35 @@
+# %%
 import geopandas as gpd
 from urllib.request import urlopen
+import os
 
-js_names = ["staniceElement","stanice"]
+# %%
+if not os.path.exists('./output/'):
+    os.makedirs('./output/')
+
+# %%
+js_names = ['staniceElement', 'stanice']
 
 for file_nam in js_names:
-    str_data = urlopen("http://portal.chmi.cz/files/portal/docs/poboc/OS/stanice/js/" + file_nam + ".js").readlines()
+    str_data = urlopen('http://portal.chmi.cz/files/portal/docs/poboc/OS/stanice/js/' + file_nam + '.js').readlines()
+    points = gpd.GeoDataFrame()
     for l in range(1,len(str_data)-1):
-        lay_nam = str(str_data[l].split(b"=")[0][4:-1], 'utf-8')
-        geojs = str(str_data[l].split(b"=")[1], 'utf-8')
-        points = gpd.read_file(geojs)
-        points.to_file(file_nam+".gpkg", layer=lay_nam, driver="GPKG")
+        geojs = str(str_data[l].split(b'=')[1], 'utf-8')
+        tmp = gpd.read_file(geojs)
+        points = points.append(tmp)
+    points.to_csv('./output/' + file_nam + '.csv', encoding='utf-8', index=False)
         
-meta = urlopen("http://portal.chmi.cz/files/portal/docs/poboc/OS/stanice/js/messages.js").readlines()
+meta = urlopen('http://portal.chmi.cz/files/portal/docs/poboc/OS/stanice/js/messages.js').readlines()
 
 for line in range(0,len(meta)):
-    meta[line] = str(meta[line],"utf-8")
+    meta[line] = str(meta[line],'utf-8')
     meta[line] = meta[line][8:]
-    meta[line] = meta[line].replace("<H4>","") 
-    meta[line] = meta[line].replace("</H4>","") 
-    meta[line] = meta[line].replace(";","") 
+    meta[line] = meta[line].replace('<H4>','') 
+    meta[line] = meta[line].replace('</H4>','') 
+    meta[line] = meta[line].replace(';','') 
 
-meta = "\n".join(meta)
+meta = '\n'.join(meta)
 
-with open("stanice_info.txt", "w") as f: 
+with open('./output/stanice_info.txt', 'w', encoding='utf-8') as f: 
     f.write(meta) 
 
