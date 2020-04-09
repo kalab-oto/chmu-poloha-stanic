68ec78cc917ba53a5a5034e909a9956231f54bef
kalab.oto@gmail.com <kalab.oto@gmail.com>
2020-04-09 22:34:32 +0200

    fix: unify quotes

--- chmu_stanice_QGIS.py
+++ chmu_stanice_QGIS.py
@@ -2,26 +2,26 @@ from qgis.core import QgsVectorLayer
 from urllib.request import urlopen
 import json
 
-js_names = ["staniceElement","stanice"]
+js_names = ['staniceElement','stanice']
 
 for file_nam in js_names:
-    str_data = urlopen("http://portal.chmi.cz/files/portal/docs/poboc/OS/stanice/js/" + file_nam + ".js").readlines()
+    str_data = urlopen('http://portal.chmi.cz/files/portal/docs/poboc/OS/stanice/js/' + file_nam + '.js').readlines()
     for l in range(1,len(str_data)-1):
-        lay_nam = str(str_data[l].split(b"=")[0][4:-1], 'utf-8')
-        geojs = str(str_data[l].split(b"=")[1], 'utf-8')
+        lay_nam = str(str_data[l].split(b'=')[0][4:-1], 'utf-8')
+        geojs = str(str_data[l].split(b'=')[1], 'utf-8')
         parsed_geojs = json.loads(geojs)
         for feat in parsed_geojs['features']:
-            feat['properties']["ELEVATION"] = float(feat['properties']["ELEVATION"])
+            feat['properties']['ELEVATION'] = float(feat['properties']['ELEVATION'])
         geojs = json.dumps(parsed_geojs)
-        iface.addVectorLayer(geojs, lay_nam, "ogr")
+        iface.addVectorLayer(geojs, lay_nam, 'ogr')
 
-# meta = urlopen("http://portal.chmi.cz/files/portal/docs/poboc/OS/stanice/js/messages.js").readlines()
+# meta = urlopen('http://portal.chmi.cz/files/portal/docs/poboc/OS/stanice/js/messages.js').readlines()
 
 # for line in range(0,len(meta)):
-    # meta[line] = str(meta[line],"utf-8")
+    # meta[line] = str(meta[line],'utf-8')
     # meta[line] = meta[line][8:]
-    # meta[line] = meta[line].replace("<H4>","") 
-    # meta[line] = meta[line].replace("</H4>","") 
-    # meta[line] = meta[line].replace(";","") 
+    # meta[line] = meta[line].replace('<H4>','') 
+    # meta[line] = meta[line].replace('</H4>','') 
+    # meta[line] = meta[line].replace(';','') 
 
-# iface.addVectorLayer(meta, "stanice_info", "ogr")
+# iface.addVectorLayer(meta, 'stanice_info', 'ogr')
